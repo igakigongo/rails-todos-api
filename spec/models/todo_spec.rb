51daae45bc12bd80b9f22480d536edc6d6f52b02
Edward Iga Kigongo <igakigongo@gmail.com>
2021-03-02 16:59:18 +0100

    add todo items controller and test, plus rubocop

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
   # Association test
   # ensure Todo model has a 1:m relationship with the Item model
-  it { should have_many(:items).dependent(:destroy) }
+  it { is_expected.to have_many(:items).dependent(:destroy) }
 
   # Validation tests
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:created_by) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:created_by) }
 end
